e3942746e432216d27aec4fac812e09e14fb568d
Steven <fanjiaxing@hotmail.com>
2014-04-17 00:24:36 +0800

    交互界面绘制
    交互界面绘制完成。需要添加事件。

--- js/nb_entity.js
+++ js/nb_entity.js
@@ -15,7 +15,13 @@ function newInstance()
         },
         "content":{
             "text":"11111111111111",
-            "image":"222222222222222"
+            "image":"222222222222222",
+            "interaction":{
+                "up":0,
+                "down":0,
+                "share_count":0,
+                "comment_count":0
+            }
         }
     }
 }
@@ -26,7 +32,7 @@ $(function(){
 
 function init_nb_entity(nbentity){
     var nb_block=$("<div/>");
-    nb_block.attr('class','nb_block');
+    nb_block.attr('class','nb_block floatl');
     createAuthor(nbentity.user).appendTo(nb_block);
     createContent(newInstance().content).appendTo(nb_block);
     $("body").append(nb_block);
@@ -62,9 +68,31 @@ function createContent(content){
        var img=$("<img/>").attr('src','http://ww3.sinaimg.cn/large/005zEVpxjw1efg0k9w69vj30cg3707dw.jpg').appendTo(a_img_content_wrapper);
 
    }
+    createInteraction(content.interaction).appendTo(div_content);
+
     return div_content;
 }
 
-function createInteraction(){
+function createInteraction(interaction){
+    var div_interaction= $("<div/>").attr("class","interaction");
+    var ul_devote=$("<ul/>").attr("class","floatl").appendTo(div_interaction);
+    var li_devote_up=$("<li/>").attr("class","devote_up").appendTo(ul_devote);
+    var a_devote_up=$("<a/>").attr("click","").appendTo(li_devote_up);
+    var span_up=$("<span/>").attr("id","").html(123).appendTo(a_devote_up);
 
+    var li_devote_down=$("<li/>").attr("class","devote_down").appendTo(ul_devote);
+    var a_devote_down=$("<a/>").attr("click","").appendTo(li_devote_down);
+    var span_down=$("<span/>").attr("id","").html(123).appendTo(a_devote_down);
+
+    var ul_interaction=$("<ul/>").attr("class","floatr").appendTo(div_interaction);
+    var li_share=$("<li/>").attr("class","share").appendTo(ul_interaction);
+    var a_share=$("<a/>").attr("click","").appendTo(li_share);
+    var span_share=$("<span/>").attr("id","").html(123).appendTo(a_share);
+
+
+    var li_common=$("<li/>").attr("class","common").appendTo(ul_interaction);
+    var a_common=$("<a/>").attr("click","").appendTo(li_common);
+    var span_common=$("<span/>").attr("id","").html(123).appendTo(a_common);
+
+    return div_interaction;
 }
